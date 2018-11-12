54ee4aa9ecb5f45b51d9d2eb321607b73106f532
Cai <caicoding2@gmail.com>
2018-11-12 00:16:45 -0600

    Credit

---
+++ Estania/Assets/Scripts/Credit/Credit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour {
+
+    public float Speed;
+  
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector2.up * Speed * Time.deltaTime);
+        
+
+        if (transform.position.y > 10)
+        {
+            transform.position = new Vector3(transform.position.x, -11f, transform.position.z);
+        }
+
+    }
+}
